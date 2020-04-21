0665e5f0dcc87e53c6a50226eab369458e174208
wildchild <cobwild_child@hotmail.com>
2020-04-21 09:18:37 +0100

    icons , theme and design changes

--- app/src/main/java/com/wildchild/locationpickermodule/locationpickermodule/Activities/LoginActivity.java
+++ app/src/main/java/com/wildchild/locationpickermodule/locationpickermodule/Activities/LoginActivity.java
@@ -8,7 +8,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.wildchild.locationpickermodule.R;
